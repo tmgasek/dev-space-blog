4d487fa0e8bda74acc4c8dffb17e002acd3309df
tmgasek <tomaszgasek28@gmail.com>
2021-12-23 14:11:54 +0000

    deployment error

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,39 +1,26 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
+import { getPosts } from '@/lib/posts';
 
-export default (req, res) => {
+export default function helloAPI(req, res) {
+  const query = req.query.q.toLowerCase();
   let posts;
 
   if (process.env.NODE_ENV === 'production') {
     // Fetch from cache
     posts = require('../../cache/data').posts;
   } else {
-    const files = fs.readdirSync(path.join('posts'));
-
-    posts = files.map((filename) => {
-      const slug = filename.replace('.md', '');
-
-      const markdownWithMeta = fs.readFileSync(
-        path.join('posts', filename),
-        'utf-8'
-      );
-
-      const { data: frontmatter } = matter(markdownWithMeta);
-
-      return {
-        slug,
-        frontmatter,
-      };
-    });
+    posts = getPosts();
   }
 
   const results = posts.filter(
     ({ frontmatter: { title, excerpt, category } }) =>
-      title.toLowerCase().indexOf(req.query.q) != -1 ||
-      excerpt.toLowerCase().indexOf(req.query.q) != -1 ||
-      category.toLowerCase().indexOf(req.query.q) != -1
+      title.toLowerCase().indexOf(query) != -1 ||
+      excerpt.toLowerCase().indexOf(query) != -1 ||
+      category.toLowerCase().indexOf(query) != -1
   );
 
   res.status(200).json(JSON.stringify({ results }));
-};
+}
